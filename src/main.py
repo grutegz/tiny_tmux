36a520ad7b5271d51fe8e14bb0add1eb0fceb6e6
grutegz <grutegz@mail.ru>
2026-05-21 17:23:38 +0500

    init

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Minimalistic terminal multiplexor for unix systems.'
+    )
+    args = parser.parse_args()
